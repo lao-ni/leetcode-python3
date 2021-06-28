bed3cec461fdb2aa9ee24e1d9af26b5604789d50
Jingyi Ni <56960103+lao-ni@users.noreply.github.com>
2021-06-28 09:57:46 +0800

    303 区域和检索 - 数组不可变

---
+++ leetcode303.py
@@ -0,0 +1,22 @@
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        if(not nums):
+            return 
+        self.dp=[0]*len(nums)
+
+        self.dp[0]=nums[0]
+        for i in range(1,len(nums)):
+            self.dp[i]=self.dp[i-1]+nums[i]
+    def sumRange(self, i: int, j: int) -> int:
+        
+        if i==0:
+            return self.dp[j]
+        else:       
+            return self.dp[j]-self.dp[i-1]
+        
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(i,j)
