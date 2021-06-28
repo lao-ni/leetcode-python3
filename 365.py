a59daeca3f60e016173b6b4db4fadea7cb4f006e
lao-ni <56960103+lao-ni@users.noreply.github.com>
2021-06-28 09:24:54 +0800

    Delete 365.py

--- 365.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def canMeasureWater(self, x: int, y: int, z: int) -> bool:
-        n=math.gcd(x,y)
-        if x+y<z:
-            return False
-        if x==0 or y==0:
-            return z==0 or z==x+y
-        return z%n==0
